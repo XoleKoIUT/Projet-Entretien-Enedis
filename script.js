dfca873843edcabbb65048f7d5214352138db8ce
XoleKo <xolekoiut76@gmail.com>
2023-01-04 14:51:45 +0100

    Avancement du site

---
+++ script.js
@@ -0,0 +1,16 @@
+/* ---------------------------------------------------------------- */
+/*                   Confirmation du mot de passe                   */
+/* ---------------------------------------------------------------- */
+
+var mdp1 = document.querySelector('.mdp1');
+var mdp2 = document.querySelector('.mdp2');
+
+mdp2.onkeyup = function(){
+    message_error = document.querySelector('.message_error');
+    /* Vérification de l'égalité */
+    if(mdp1.value != mdp2.value){
+        message_error.innerText = "Mdp confirmation non conforme";
+    } else{
+        message_error.innerText = ""; /* Car les mots de passes sont conformes entre eux */
+    }
+}
